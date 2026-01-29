9cbddc5dbf14c3877257d018d801fbe3f0c11344
jeanbenoit-richez <jeanbenoit@realt.co>
2026-01-29 15:09:39 +0200

    feat: filter social button icon in production

--- packages/@real-token/aa-modal/src/hooks/useSocialConnectors.ts
+++ packages/@real-token/aa-modal/src/hooks/useSocialConnectors.ts
@@ -1,6 +1,22 @@
-import { useWeb3AuthPrivate } from "@real-token/web3";
+import { useWeb3AuthPrivate, useRealTokenWeb3Config } from "@real-token/web3";
+import { useMemo } from "react";
 
 export const useSocialConnectors = () => {
   const privateWeb3auth = useWeb3AuthPrivate();
-  return privateWeb3auth.loginModal.uiConfig.loginMethodsOrder;
+  const { authProviderConfig } = useRealTokenWeb3Config();
+
+  const allConnectors =
+    privateWeb3auth.loginModal.uiConfig.loginMethodsOrder;
+
+  return useMemo(() => {
+    const configuredProviders = Object.keys(authProviderConfig ?? {});
+
+    if (configuredProviders.length === 0) {
+      return allConnectors;
+    }
+
+    return allConnectors.filter((connector) =>
+      configuredProviders.includes(connector)
+    );
+  }, [allConnectors, authProviderConfig]);
 };
